ac2561fc367493e42153ee7242a11679242e91e7
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2016-11-07 00:50:02 -0500

    uportregister owner

--- contracts/UportRegistry.sol
+++ contracts/UportRegistry.sol
@@ -4,17 +4,28 @@ contract UportRegistry {
 
   uint public version;
   address public previousPublishedVersion;
+  address public owner;
+  modifier onlyOwner {
+      if(msg.sender != owner)
+        throw;
+      _;
+  }
 
   mapping(address => bytes) public ipfsAttributeLookup;
 
   function UportRegistry(address _previousPublishedVersion) {
     version = 1;
     previousPublishedVersion = _previousPublishedVersion;
+    owner = msg.sender;
   }
 
-  function setAttributes(bytes ipfsHash) {
-    ipfsAttributeLookup[msg.sender] = ipfsHash;
-    AttributesSet(msg.sender, now);
+  function transferOwnership(address newOwner) onlyOwner {
+      owner = newOwner;
+  }
+
+  function setAttributes(address personaAddress, bytes ipfsHash) onlyOwner {
+    ipfsAttributeLookup[personaAddress] = ipfsHash;
+    AttributesSet(personaAddress, now);
   }
 
   function getAttributes(address personaAddress) constant returns(bytes) {
